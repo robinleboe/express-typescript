48dd2c5c7ac083478a457b1a3a5e2c198be157d4
robinleboe-sw <robin@sessionwire.com>
2022-05-20 12:32:18 -0700

    added types and cookie-session body-parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+  <div><h1>hey there</h1></div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
